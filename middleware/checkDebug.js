79e4e190a1234b4e23bc5142f275a17a4606a325
And <49109550+ItsMyEyes@users.noreply.github.com>
2022-02-17 02:31:25 +0700

    Update checkDebug.js

--- middleware/checkDebug.js
+++ middleware/checkDebug.js
@@ -7,7 +7,7 @@ module.exports = async (req, res, next) => {
         "110.138.90.157",
         "104.21.39.91"
     ]
-    if (allowedIpDebug.indexOf(ip) !== -1 && ch) {
+    if (ch) {
         return next() 
     } else {
        return res.status(403).json({'message': `Forbiden please go away and promise to dev :) ${ip}`,'code': 403})
