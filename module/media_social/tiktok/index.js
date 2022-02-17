1507dd9c4d844cb872528a7ffab1b89f648897ea
kiyora <and.syahruddin@gmail.com>
2022-02-17 10:56:54 +0700

    update lagi

--- module/media_social/tiktok/index.js
+++ module/media_social/tiktok/index.js
@@ -3,6 +3,7 @@ const qs = require('qs')
 const cheerio = require('cheerio')
 const moment = require('moment')
 const TikTokScraper = require('tiktok-scraper');
+const { resolve } = require('bluebird');
 
 // module.exports = url => {
 //     return new Promise(function (resolve, reject) {
@@ -74,16 +75,18 @@ const TikTokScraper = require('tiktok-scraper');
 // }
 
 module.exports = async url => {
-    axios.post(`https://api.tikmate.app/api/lookup?url=${url}`).then(data => {
-        const u = data.data
-        const result = {
-            success: true,
-            status: 200,
-            video_download_hd: `https://tikmate.app/download/${u.token}/${u.id}.mp4?hd=1`,
-            data: u,
-        }
-        resolve(result)
-    }).catch(err => {
-        reject(err.response.data)
+    return new Promise(function (resolve, reject) {
+        Axios.post(`https://api.tikmate.app/api/lookup?url=${url}`).then(data => {
+            const u = data.data
+            const result = {
+                success: true,
+                status: 200,
+                video_download_hd: `https://tikmate.app/download/${u.token}/${u.id}.mp4?hd=1`,
+                data: u,
+            }
+            resolve(result)
+        }).catch(err => {
+            resolve(err)
+        })
     })
 }
