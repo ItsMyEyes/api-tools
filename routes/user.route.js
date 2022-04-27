3788e64649919c6baeb0b8825fdc78259f8a9e08
kiyora <and.syahruddin@gmail.com>
2022-04-27 12:00:36 +0700

    puda

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,6 +4,6 @@ const userCr = require('../controller/user.controller')
 const middle = require('../middleware/admin')
 
 router.post('/login', userCr.login)
-router.post('/reg', middle, userCr.registUser)
+router.post('/reg', userCr.registUser)
 
 module.exports = router
